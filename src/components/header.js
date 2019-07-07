b344f3f377caae35250918250e25d4f7c7117ca5
Humberd <humberd2@gmail.com>
2019-07-07 22:20:41 +0200

    Added header

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './header.scss';
+
+export const Header = () => (
+    <header className="header">
+      <h1 className="name">
+        Maciej Sawicki
+      </h1>
+      <p className="profession">
+        Fullstack Engineer
+      </p>
+
+      <p className="description">
+        Fullstack Engineer with Master's degree in Computer Science mostly focued on Angular. Fluent with a great number of languages and technologies both on a frontend and a backend side. Is able to create an application from top to bottom and expose it to the world.
+      </p>
+    </header>
+);
