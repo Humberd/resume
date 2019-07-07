92545c137b79ebbafee984ce0a4c2748b68dd747
Humberd <humberd2@gmail.com>
2019-07-07 21:51:06 +0200

    Save commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,48 +5,22 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import Header from "./header"
-import "./layout.css"
+import './reset.scss';
+import './layout.scss';
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = ({children}) => {
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+      <main className="content">
+        {children}
+      </main>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {Link} from 'gatsby';
 
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
 
-import("index.scss");
+import('./index.scss');
 
 const IndexPage = () => (
     <Layout>
@@ -16,7 +15,6 @@ const IndexPage = () => (
       <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
         <Image/>
       </div>
-      <Link to="/page-2/">Go to page 2</Link>
     </Layout>
 );
 
