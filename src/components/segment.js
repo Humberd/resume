1f31facdb1ee991e1071a30a46d822dada3cef49
Humberd <humberd2@gmail.com>
2019-07-11 21:20:19 +0200

    Save commit

--- src/components/segment.js
+++ src/components/segment.js
@@ -1,7 +1,19 @@
+import React from 'react';
+
 import './segment.scss';
+import svg from '../images/person.svg';
 
 export const Segment = () => (
-    <section>
-        foo
+    <section className="segment">
+        <SegmentHeader/>
     </section>
 );
+
+export const SegmentHeader = async () => {
+
+  return (
+      <header className="segment--header">
+    <img src={svg} alt=""/>
+    <h1>Personal Info</h1>
+  </header>
+)};
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout';
 
 import './index.scss';
 import {Header} from '../components/header';
+import {Segment} from '../components/segment';
 
 const IndexPage = () => (
     <Layout>
@@ -11,12 +12,7 @@ const IndexPage = () => (
 
       <div className="columns">
         <div className="column">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(() => (
-              <div className="foobar">
-                Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie
-                niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker
-              </div>
-          ))}
+          <Segment></Segment>
         </div>
         <div className="column">
           hello
