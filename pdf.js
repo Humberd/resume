9e51e864bb70a466dbe1cfc82d8eb998745b82f0
humberd <humberd2@gmail.com>
2020-06-25 21:56:58 +0200

    Fixed handing pdf generation process

--- pdf.js
+++ pdf.js
@@ -5,27 +5,31 @@ const MARGIN = '1cm';
 const isCI = (process.argv[2] || '').toLowerCase() === 'ci';
 const port = isCI ? '9000' : '8000';
 
-(async () => {
+const main = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`http://localhost:${port}`, {waitUntil: ['domcontentloaded', 'networkidle2']});
-  await page.pdf({
-    path: 'Maciej Sawicki - Resume.pdf',
-    format: 'A4',
-    printBackground: true,
-    margin: {
-      top: MARGIN,
-      right: MARGIN,
-      bottom: MARGIN,
-      left: MARGIN,
-    },
-    displayHeaderFooter: true,
-    headerTemplate: `<div></div>`,
-    footerTemplate: footerTemplate,
-  });
+  try {
+    await page.goto(`http://localhost:${port}`, {waitUntil: ['domcontentloaded', 'networkidle2']});
+    await page.pdf({
+      path: 'Maciej Sawicki - Resume.pdf',
+      format: 'A4',
+      printBackground: true,
+      margin: {
+        top: MARGIN,
+        right: MARGIN,
+        bottom: MARGIN,
+        left: MARGIN,
+      },
+      displayHeaderFooter: true,
+      headerTemplate: `<div></div>`,
+      footerTemplate: footerTemplate,
+    });
+  } finally {
+    await browser.close();
+  }
+};
 
-  await browser.close();
-})();
+main()
 
 // language=HTML
 const footerTemplate = `
