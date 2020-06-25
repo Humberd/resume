565e1fcb8927d98477d8b1cff874f5a6e9efe7e4
humberd <humberd2@gmail.com>
2020-06-25 21:49:41 +0200

    Fixed generating pdf in ci

--- pdf.js
+++ pdf.js
@@ -2,10 +2,13 @@ const puppeteer = require('puppeteer');
 
 const MARGIN = '1cm';
 
+const isCI = (process.argv[2] || '').toLowerCase() === 'ci';
+const port = isCI ? '9000' : '8000';
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://localhost:8000', {waitUntil: ['domcontentloaded', 'networkidle2']});
+  await page.goto(`http://localhost:${port}`, {waitUntil: ['domcontentloaded', 'networkidle2']});
   await page.pdf({
     path: 'Maciej Sawicki - Resume.pdf',
     format: 'A4',
