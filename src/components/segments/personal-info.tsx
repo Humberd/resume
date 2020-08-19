8e63b2b405b9be6ce9b63baa22cbafe830b183de
humberd <humberd2@gmail.com>
2020-08-19 21:13:50 +0200

    Update: Personal info now has no labels
    Darkened a bit theme color.
    Applied theme color to other headers

--- src/components/segments/personal-info.tsx
+++ src/components/segments/personal-info.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import './personal-info.scss';
-import { getIcon } from '../utils';
+import * as React from "react"
+import "./personal-info.scss"
+import { getIcon } from "../utils"
 
 export interface PersonalInfoProps {
   entries: PersonalInfoEntryProps[];
@@ -17,36 +17,29 @@ export const PersonalInfo = (props: PersonalInfoProps) => (
 
 export interface PersonalInfoEntryProps {
   title: string;
-  icon?: string;
+  icon: string;
   value: string;
-  href?: string;
+  href: string;
 }
 
 const PersonalInfoEntry = (props: PersonalInfoEntryProps) => {
-  let value: React.ReactNode;
-  if (props.href) {
-    value =
-        <a
-            href={props.href}
-            target={props.href.startsWith('http') ? '_blank' : ''}
-            rel="noopener noreferrer"
-            aria-label={props.title}
-        >{props.value}</a>;
-  } else {
-    value = <span>{props.value}</span>;
-  }
-
   return (
       <li className="personal-info-entry">
-        <label className="title">
-          {props.icon && <img
-              className="title-image"
-              src={getIcon(props.icon)}
-              aria-hidden={true}
-          />}
-          <p>{props.title}</p>
-        </label>
-        {value}
+        <a
+          href={props.href}
+          target={props.href.startsWith('http') ? '_blank' : ''}
+          rel="noopener noreferrer"
+          className="link"
+        >
+          <img
+            className="title-image"
+            src={getIcon(props.icon)}
+            aria-hidden={true}
+            title={props.title}
+          />
+          <span>{props.value}</span>
+        </a>
+
       </li>
   );
 };
