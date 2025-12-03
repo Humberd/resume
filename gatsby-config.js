1a62b8ea99efadc6230091f77461c6bb8021370e
humberd <humberd2@gmail.com>
2025-12-03 20:34:37 +0100

    Fix compilation warnings

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        sassOptions: {
+          api: 'modern-compiler',
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-typescript`,
       options: {
